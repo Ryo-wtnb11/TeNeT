3e5b31d6e6c214c2333d77a82eab6bf3d500cd8b
Ryo-wtnb11 <ryo0616mani@gmail.com>
2026-07-31 16:18:57 +0900

    Drop unused opposite null temporaries

--- tenet-matrixalgebra/src/factorize.rs
+++ tenet-matrixalgebra/src/factorize.rs
@@ -4479,7 +4479,7 @@ where
             kept: null_dim,
             left: u[rows * rank..].to_vec(),
             left_rows: rows,
-            right: vec![D::zero(); null_dim * cols],
+            right: Vec::new(),
             right_leading: null_dim,
         });
     }
@@ -4546,7 +4546,7 @@ where
         pairs.push(FactorPair {
             sector: matrix.sector,
             kept: null_dim,
-            left: vec![D::zero(); rows * null_dim],
+            left: Vec::new(),
             left_rows: rows,
             right: adjoint_col_major(&v[cols * rank..], cols, null_dim),
             right_leading: null_dim,
