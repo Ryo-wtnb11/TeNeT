d89fc90e45d6b6a9b28a7532ae5cd567841e62c0
Ryo-wtnb11 <ryo0616mani@gmail.com>
2026-09-17 18:34:26 +0200

    refactor: format recorded EIGH spectra

--- tenet-matrixalgebra/src/tests.rs
+++ tenet-matrixalgebra/src/tests.rs
@@ -827,8 +827,7 @@ impl DenseExecutor for RecordingEigh {
 
     fn eigh(&mut self, input: DenseRead<'_>) -> Result<Vec<DenseTensor>, DenseError> {
         let outputs = self.inner.eigh(input)?;
-        self.raw_values
-            .push(outputs[0].as_f64_slice()?.to_vec());
+        self.raw_values.push(outputs[0].as_f64_slice()?.to_vec());
         Ok(outputs)
     }
 
